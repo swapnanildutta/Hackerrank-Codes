c5d7dba448f890851bd0fc1f1792a62b0fff900d
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2020-10-04 11:52:37 +0530

    Update LeftRotation.py

--- Data Structures/Arrays/LeftRotation.py
+++ Data Structures/Arrays/LeftRotation.py
@@ -1,4 +1,6 @@
 """
+Left Rotation
+
 A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. Given an integer, , rotate the array that many steps left and return the result.
 
 Example
