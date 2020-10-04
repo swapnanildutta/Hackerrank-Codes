dc478255323f1128cc0e16bcfea94d045949c2d5
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2020-10-04 11:44:26 +0530

    Create LeftRotation.py

---
+++ Data Structures/Arrays/LeftRotation.py
@@ -0,0 +1,67 @@
+'''
+A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. Given an integer, , rotate the array that many steps left and return the result.
+
+Example
+
+
+After  rotations, .
+
+Function Description
+
+Complete the rotateLeft function in the editor below.
+
+rotateLeft has the following parameters:
+
+int d: the amount to rotate by
+int arr[n]: the array to rotate
+Returns
+
+int[n]: the rotated array
+Input Format
+
+The first line contains two space-separated integers that denote , the number of integers, and , the number of left rotations to perform.
+The second line contains  space-separated integers that describe .
+
+Constraints
+
+Sample Input
+
+5 4
+1 2 3 4 5
+Sample Output
+
+5 1 2 3 4
+Explanation
+
+To perform  left rotations, the array undergoes the following sequence of changes:
+  [1,2,3,4,5] -> [2,3,4,5,1] -> [3,4,5,1,2] -> [4,5,1,2,3] -> [5,1,2,3,4]
+
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    d = d%len(a)
+    
+    l2 = a[:d]
+    l1 = a[d:]
+    l1 = l1+l2
+
+    print(*l1)
+
