ed68c01c8c274e97d90de02b0d4134ca86d5e6fc
tarunsingh7379 <72147696+tarunsingh7379@users.noreply.github.com>
2020-10-04 11:00:22 +0530

    code for finding Palindrome String in c

---
+++ C/Palindrome String.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char a[25],b[25];
+	int c;
+	printf("Enter a string:");
+	gets(a);
+	strcpy(b,a);//Copying input string
+	strrev(a);//Reverse the input string
+	c=strcmp(a,b);//Comparing the reverse string with the copy of the input string
+	if(c==0)
+	{
+		printf("Entered string is a palindrome.");
+	}
+	else
+	{
+		printf("Entered string is not a palindrome.");
+	}
+}
