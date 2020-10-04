1ca93c9930889e6d150942739176de4b18bfc77f
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-10-05 00:17:08 +0530

    Add Maximum_Height_of_Triangle solution

---
+++ Python/Maximum_Height_OF_Triangle.py
@@ -0,0 +1,13 @@
+'''
+Given integers b and a , find the smallest integer h, such that there exists 
+a triangle of height h, base b, having an area of at least a.
+
+link : https://www.hackerrank.com/challenges/lowest-triangle/problem
+'''
+import math
+b, a = map(int, input().split())
+h = (2*a)/(b)
+if h > int(h):
+    print(math.ceil(h))
+else:
+    print("%.0f" % h)
