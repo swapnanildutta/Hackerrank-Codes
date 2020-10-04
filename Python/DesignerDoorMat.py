4c99a33d48dbfd88b464c6f45294e2e629d5ca08
fharook shaik <fharookshaik.5@gmail.com>
2020-10-04 14:08:05 +0530

    Added two programs 1. Designer Door Mat 2. String Split and Join

---
+++ Python/DesignerDoorMat.py
@@ -0,0 +1,12 @@
+n,m = map(int,input().split(" "))
+str = "WELCOME"
+symbol = ".|."
+for i in range(0,n):
+    if i % 2 != 0:
+        print((symbol*i).center(m,"-"))
+print(str.center(m,"-"))
+i = n-1
+while i>=0:
+    if i % 2 != 0:
+        print((symbol*i).center(m,"-"))
+    i-=1
