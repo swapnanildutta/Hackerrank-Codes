5b85b056238ee276ede3dcc732e17ae18847b3b7
AthulJoseph <55750689+AthulJoseph27@users.noreply.github.com>
2020-10-04 10:23:51 +0530

    Create TheMaximumSubarray.cpp

---
+++ CPP/TheMaximumSubarray.cpp
@@ -0,0 +1,160 @@
+/*
+We define subsequence as any subset of an array. We define a subarray as a contiguous subsequence in an array.
+
+Given an array, find the maximum possible sum among:
+
+all nonempty subarrays.
+all nonempty subsequences.
+Print the two values as space-separated integers on one line.
+
+Note that empty subarrays/subsequences should not be considered.
+
+For example, given an array , the maximum subarray sum is comprised of element inidices  and the sum is . The maximum subsequence sum is comprised of element indices  and the sum is .
+
+Function Description
+
+Complete the maxSubarray function in the editor below. It should return an array of two integers: the maximum subarray sum and the maximum subsequence sum of .
+
+maxSubarray has the following parameter(s):
+
+arr: an array of integers
+Input Format
+
+The first line of input contains a single integer , the number of test cases.
+
+The first line of each test case contains a single integer .
+The second line contains  space-separated integers  where .
+
+Constraints
+
+The subarray and subsequences you consider should have at least one element.
+
+Output Format
+
+Print two space-separated integers denoting the maximum sums of nonempty subarrays and nonempty subsequences, respectively.
+
+Sample Input 0
+
+2
+4
+1 2 3 4
+6
+2 -1 2 3 4 -5
+Sample Output 0
+
+10 10
+10 11
+Explanation 0
+
+In the first case: The maximum sum for both types of subsequences is just the sum of all the elements since they are all positive.
+
+In the second case: The subarray  is the subarray with the maximum sum, and  is the subsequence with the maximum sum.
+
+Sample Input 1
+
+1
+5
+-2 -3 -1 -4 -6
+Sample Output 1
+
+-1 -1
+Explanation 1
+
+Since all of the numbers are negative, both the maximum subarray and maximum subsequence sums are made up of one element, .
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the maxSubarray function below.
+vector<int> maxSubarray(vector<int> arr) {
+    int best = 0,a = 0,best_seq = 0;
+
+    for(int i:arr){
+        a = max(a+i,0);
+        best_seq += max(i,0);
+        best = max(best,a);
+    }
+
+    if(best_seq == 0) best_seq = *max_element(arr.begin(),arr.end());
+    if(best == 0) best = *max_element(arr.begin(),arr.end());
+
+    return {best,best_seq};
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector<string> arr_temp = split_string(arr_temp_temp);
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
+        }
+
+        vector<int> result = maxSubarray(arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
