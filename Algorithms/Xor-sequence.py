227ce8239408a564dbd9a9e1b90d3fe7ccabc343
godspeed5 <36200106+godspeed5@users.noreply.github.com>
2020-10-04 17:51:00 +0530

    Create Xor-sequence.py

---
+++ Algorithms/Xor-sequence.py
@@ -0,0 +1,39 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the xorSequence function below.
+def xorSequence(l, r):
+    def A(x):
+        a = x%8
+        if(a == 0 or a == 1):
+            return x
+        if(a == 2 or a == 3):
+            return 2
+        if(a == 4 or a == 5):
+            return x+2
+        if(a == 6 or a == 7):
+            return 0;
+    ans = A(l-1)^A(r)
+    return ans
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        lr = input().split()
+
+        l = int(lr[0])
+
+        r = int(lr[1])
+
+        result = xorSequence(l, r)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
