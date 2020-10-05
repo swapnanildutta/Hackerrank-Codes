d3db01abf07f0c8819791d4c0ad2da1af38fa508
dikshajoshi18 <dikshajoshi1800@gmail.com>
2020-10-05 11:12:30 +0530

    done changes

--- Python/Sales by Match.py
+++ Python/Sales by Match.py
@@ -1,3 +1,5 @@
+#problem hackerrank.com/challenges/sock-merchant/problem
+#points 10
 '''Alex works at a clothing store. There is a large pile of socks that must be paired by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
 For example, there are  socks with colors . There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
