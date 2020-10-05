db9e942c2d6ad2bc4145d7b4d83f47f9190d8f88
Ankit Ranjan <64700993+ankitranjan9@users.noreply.github.com>
2020-10-05 15:11:37 +0530

    Update Capitalize!.py

--- Python/Capitalize!.py
+++ Python/Capitalize!.py
@@ -31,16 +31,15 @@ Chris Alan
 
 '''
 
-def solve(s):
-    return s.title()
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    s = input()
-
-    result = solve(s)
-
-    fptr.write(result + '\n')
-
-    fptr.close()
+def getCap(s):
+    s = ' ' + s
+    r = ''
+    for i in range(1, len(s)):
+        if s[i - 1] == ' ' and s[i] != ' ':
+            r += s[i].upper()
+        else:
+            r += s[i]
+    return r
+    
+s = input()
+print(getCap(s))
