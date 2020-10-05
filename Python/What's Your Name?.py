9cc7bd86f9ea80fdfc05921b54b0710f8af91e1c
sathvick <sunnysathvick5267@gmail.com>
2020-10-05 20:16:41 +0530

    added Whats your name py file

---
+++ Python/What's Your Name?.py
@@ -0,0 +1,9 @@
+def print_full_name(a, b):
+
+    b = b+"!"
+    print("Hello",a, b,"You just delved into python.");
+    
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
