e225fd0c5e6dfe48b16e413017cd775eeca125f1
prackode <72190853+prackode@users.noreply.github.com>
2020-10-05 23:36:16 +0530

    Create Climbing_the_Leaderboard.py

---
+++ Python/Climbing_the_Leaderboard.py
@@ -0,0 +1,49 @@
+'''
+Problem Description:
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+An arcade game player wants to climb to the top of the leaderboard and track their ranking. The game uses Dense Ranking, so its leaderboard works like this:
+
+The player with the highest score is ranked number 1 on the leaderboard.
+Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
+Example
+* ranked=[100,90,90,80]
+* player=[70,80,105]
+The ranked players will have ranks 1, 2, 2, and 3, respectively. If the player's scores are 70, 80 and 105, their rankings after each game are 4th, 3rd and 1st. Return [4,3,1].
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the climbingLeaderboard function below.
+def climbingLeaderboard(scores, alice):
+    scores=sorted(list(set(scores)))
+    j=0
+    rank=[]
+    n=len(scores)
+    for i in alice:
+        while(n>j and i>=scores[j]):
+            j+=1
+        rank.append(n+1-j)
+    return(rank)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    scores_count = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    alice_count = int(input())
+
+    alice = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(scores, alice)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
