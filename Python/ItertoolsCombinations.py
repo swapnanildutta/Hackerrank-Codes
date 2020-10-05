e98b8ea2d596114cf36acffa59a9259a26dae7d4
Gautam Kumar <32988728+gautam08@users.noreply.github.com>
2020-10-06 02:14:10 +0530

    Updated Itertools file

--- Python/ItertoolsCombinations.py
+++ Python/ItertoolsCombinations.py
@@ -1,8 +1,6 @@
 '''
 itertools.combinations(iterable, r)
-This tool returns the
-
-length subsequences of elements from the input iterable.
+This tool returns the length subsequences of elements from the input iterable.
 
 Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
 
@@ -17,31 +15,23 @@ Sample Code
 >>> print list(combinations(A,4))
 [(1, 1, 3, 3), (1, 1, 3, 3), (1, 1, 3, 3), (1, 3, 3, 3), (1, 3, 3, 3)]
 
-Task
 
-You are given a string
-.
-Your task is to print all possible combinations, up to size
+-------------------------------------------------------------------------<< Task >>----------------------------------------------------------------------------------------------
 
-, of the string in lexicographic sorted order.
+You are given a string.
+Your task is to print all possible combinations, up to size of the string in lexicographic sorted order.
 
-Input Format
+Input Format >>
 
-A single line containing the string
-and integer value
+A single line containing the string and integer value separated by a space.
 
-separated by a space.
-
-Constraints
-
+Constraints >>
 
 The string contains only UPPERCASE characters.
 
-Output Format
+Output Format >>
 
-Print the different combinations of string
-
-on separate lines.
+Print the different combinations of string on separate lines.
 
 Sample Input
 
