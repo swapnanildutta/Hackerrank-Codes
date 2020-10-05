8f1c7e0d4554be27a9ce6ecd6c54c45507c25ec4
Sazin Rishad Samin <sazinsamin50@gmail.com>
2020-10-05 22:33:58 +0600

    Update PlayingWithCharacters.c

--- C/PlayingWithCharacters.c
+++ C/PlayingWithCharacters.c
@@ -1,53 +1,27 @@
-#https://www.hackerrank.com/challenges/playing-with-characters/problem
-#Input a character,string and sentence and print it
+# https://www.hackerrank.com/challenges/playing-with-characters/problem
+# Input a character,string and sentence and print it
 
-/*Objective
 
-This challenge will help you to learn how to take a character, a string and a sentence as input in C.
+# Task
 
-To take a single character ch as input, you can use scanf("%c", &ch ); and printf("%c", ch) writes a character specified by the argument char to stdout
+# You have to print the character, ch, in the first line. Then print  in next line. In the last line print the sentence sen, .
 
-char ch;
-scanf("%c", &ch);
-printf("%c", ch);
-This piece of code prints the character ch.
+#Input Format
 
-You can take a string as input in C using scanf(“%s”, s). But, it accepts string only until it finds the first space.
+#First, take a character, ch as input.
+#Then take the string,s as input.
+#Lastly, take the sentence sen as input.
 
-In order to take a line as input, you can use scanf("%[^\n]%*c", s); where s is defined as char s[MAX_LEN] where MAX_LEN is the maximum size of s. Here, [] is the scanset character. ^\n stands for taking input until a newline isn't encountered. Then, with this %*c, it reads the newline character and here, the used * indicates that this newline character is discarded.
+#Constraints
 
-Note: The statement: scanf("%[^\n]%*c", s); will not work because the last statement will read a newline character, \n, from the previous line. This can be handled in a variety of ways. One way is to use scanf("\n"); before the last statement.
+#Strings for s and sen will have fewer than 100 characters, including the newline.
 
-Task
+#Output Format
 
-You have to print the character, ch, in the first line. Then print  in next line. In the last line print the sentence sen, .
+#Print three lines of output. The first line prints the character, ch.
+#The second line prints the string, s.
+#The third line prints the sentence, sen.
 
-Input Format
-
-First, take a character, ch as input.
-Then take the string,s as input.
-Lastly, take the sentence sen as input.
-
-Constraints
-
-Strings for s and sen will have fewer than 100 characters, including the newline.
-
-Output Format
-
-Print three lines of output. The first line prints the character, ch.
-The second line prints the string, s.
-The third line prints the sentence, sen.
-
-Sample Input 0
-
-C
-Language
-Welcome To C!!
-Sample Output 0
-
-C
-Language
-Welcome To C!!*/
 
 
 #include<stdio.h>
