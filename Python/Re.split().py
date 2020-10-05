b3da6b608a1a0724d70271bfbf4d2e0f403457ca
sathvick <sunnysathvick5267@gmail.com>
2020-10-05 20:43:58 +0530

    added Calendar module py file

---
+++ Python/Re.split().py
@@ -0,0 +1,30 @@
+'''
+You are given a string
+
+consisting only of digits 0-9, commas ,, and dots .
+
+Your task is to complete the regex_pattern defined below, which will be used to re.split() all of the , and . symbols in
+
+.
+
+It’s guaranteed that every comma and every dot in
+
+is preceeded and followed by a digit.
+
+Sample Input 0
+
+100,000,000.000
+
+Sample Output 0
+
+100
+000
+000
+000
+
+
+
+'''
+regex_pattern = r"[.,]+"   
+import re
+print("\n".join(re.split(regex_pattern, input())))
