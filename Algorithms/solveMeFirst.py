ddc52c496c2e0e7f90a7c401503ddd9d0f4f0d34
ANURANJAN PANDEY <anuranjanpandey5@gmail.com>
2020-10-05 11:57:36 +0530

    Updated SolveMeFirst.py

--- Algorithms/solveMeFirst.py
+++ Algorithms/solveMeFirst.py
@@ -1,3 +1,19 @@
+'''Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int a, int b);
+
+where,
+
+a is the first integer input.
+b is the second integer input
+Return values
+
+sum of the above two integers
+'''
+
+
 def solveMeFirst(a, b):
     # Hint: Type return a+b below
     return a + b
