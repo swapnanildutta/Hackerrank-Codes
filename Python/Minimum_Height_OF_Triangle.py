ae018cefec2931185c6f64c38267068a4e86079d
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-10-05 09:56:09 +0530

    Add minimum height of triangle in python

--- Python/Minimum_Height_OF_Triangle.py
+++ Python/Minimum_Height_OF_Triangle.py
@@ -1,12 +1,15 @@
 '''
-Given integers b and a , find the smallest integer h, such that there exists 
-a triangle of height h, base b, having an area of at least a.
+Given integers b and a , find the smallest integer h, such that there exists a triangle of height h, base b, having an area of at least a.
 
 link : https://www.hackerrank.com/challenges/lowest-triangle/problem
+
 '''
 import math
+
 b, a = map(int, input().split())
+
 h = (2*a)/(b)
+
 if h > int(h):
     print(math.ceil(h))
 else:
