5f11eb9c638742d16cb2b87d29c1c58b0a12db8f
PratikshaGoyal <chgoyal.prt@gmail.com>
2020-10-05 10:56:29 +0530

    Updated solution to beautiful days at the movies

--- Python/BeautifulDaysAtTheMovies.py
+++ Python/BeautifulDaysAtTheMovies.py
@@ -1,14 +1,17 @@
 #!/bin/python3
 
 '''
+Problem Link: https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+
+Problem Description:
+
 Lily likes to play games with integers. She has created a new game where she determines the difference between a number and its reverse. For instance, given the number 12 , its reverse is 21. Their difference is 9. The number 120 reversed is 21, and their difference is 99.
 
 She decides to apply her game to decision making. She will look at a numbered range of days and will only go to a movie on a beautiful day.
 
 Given a range of numbered days, [i...j] and a number k, determine the number of days in the range that are beautiful. Beautiful numbers are defined as numbers where |i-reverse(i)| is evenly divisible by k. If a day's value is a beautiful number, it is a beautiful day. Print the number of beautiful days in the range.
 
-Function Description
-
+Function Description:
 Complete the beautifulDays function in the editor below. It must return the number of beautiful days in the range.
 
 beautifulDays has the following parameter(s):
@@ -16,23 +19,21 @@ beautifulDays has the following parameter(s):
 i: the starting day number
 j: the ending day number
 k: the divisor
-Input Format
 
+Input Format:
 A single line of three space-separated integers describing the respective values of i, j, and k.
 
-Constraints
+Constraints:
 1) 1<=j<=j<=2*10^6
 2) 1<=k<=2*10^9
 
-Output Format
-
+Output Format:
 Print the number of beautiful days in the inclusive range between i and j.
 
-Sample Input
-
+Sample Input:
 20 23 6
-Sample Output
 
+Sample Output:
 2
 '''
 
