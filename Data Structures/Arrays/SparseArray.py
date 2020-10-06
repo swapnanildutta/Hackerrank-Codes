3a7b2ee64c9318290aa95b7f04c973f2b73120bd
Aathira <aathirasatheeshnair@gmail.com>
2020-10-06 21:54:55 +0530

    sparsearray

---
+++ Data Structures/Arrays/SparseArray.py
@@ -0,0 +1,46 @@
+
+
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the matchingStrings function below.
+def matchingStrings(strings, queries):
+    reslis=[]
+   
+    for x in queries:
+        count=0
+        if x in strings:
+            count=strings.count(x)
+        reslis.append(count)
+    return reslis        
+
+
+
+if __name__ == '__main__':
+    
+
+    strings_count = int(input())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    print(res)
