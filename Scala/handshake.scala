4cc2bc7d788cca0074b309f76722196b05275905
suryaprakashsahu <suryathe16@gmail.com>
2020-10-06 09:33:46 +0530

    issue #2 add handshake problem description

--- Scala/handshake.scala
+++ Scala/handshake.scala
@@ -1,3 +1,11 @@
+/*
+Hackerrank problem : https://www.hackerrank.com/challenges/handshake/problem
+At the annual meeting of Board of Directors of Acme Inc. If everyone attending shakes hands exactly one time with every other attendee, how many handshakes are there?
+
+Example
+n = 3
+There are attendees, p1, p2 and p3. p1 shakes hands with p2 and p3, and p2 shakes hands with p3. Now they have all shaken hands after 3 handshakes. 
+*/
 import java.io.PrintWriter
 
 object Solution {
