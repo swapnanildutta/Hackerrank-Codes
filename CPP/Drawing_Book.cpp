e560c7f735340a8b19ff0cd718b29ebd584567c9
rgarg-2711 <rgarg1099@gmail.com>
2020-10-06 16:47:41 +0530

    update Drawing_Book Problem

--- CPP/Drawing_Book.cpp
+++ CPP/Drawing_Book.cpp
@@ -1,4 +1,6 @@
 /*
+quesn:https://www.hackerrank.com/challenges/drawing-book/problem
+Problem:
 A teacher asks the class to open their books to a page number. A student can either start turning pages from the front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page  is always on the right side:
 
 image
