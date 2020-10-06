f3c7dba14639d581085466920c8b12ac1eb41058
parthzz <55730488+parth93QA@users.noreply.github.com>
2020-10-06 09:59:40 +0530

    Made the changes

--- Python/birthdaycakecandles.py
+++ Python/birthdaycakecandles.py
@@ -1,52 +1,50 @@
-'''You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+# You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
 
-Example
+# Example
 
-candles = [4,4,1,3]
+# candles = [4,4,1,3]
 
-The maximum height candles are  4 units high. There are 2  of them, so return 2.
+# The maximum height candles are  4 units high. There are 2  of them, so return 2.
 
-Function Description
+# Function Description
 
-Complete the function birthdayCakeCandles in the editor below.
+# Complete the function birthdayCakeCandles in the editor below.
 
-birthdayCakeCandles has the following parameter(s):
+# birthdayCakeCandles has the following parameter(s):
 
- ---int candles[n]: the candle heights
+#  ---int candles[n]: the candle heights
 
 
-Returns
+# Returns
 
- ---int:  the number of candles that are tallest
+#  ---int:  the number of candles that are tallest
 
-Input Format
+# Input Format
 
-The first line contains a single integer,n , the size of candles[].
-The second line contains n space-separated integers, where each integer i  describes the height of candles[i].
+# The first line contains a single integer,n , the size of candles[].
+# The second line contains n space-separated integers, where each integer i  describes the height of candles[i].
 
-Constraints
- --- 1<=n<=10^5
- --- 1<=candles[i]<=10^7
+# Constraints
+#  --- 1<=n<=10^5
+#  --- 1<=candles[i]<=10^7
 
-Sample Input 0
+# Sample Input 0
 
-4
-3 2 1 3
-Sample Output 0
+# 4
+# 3 2 1 3
+# Sample Output 0
 
-2
+# 2
 
-Explanation 0
+# Explanation 0
 
-Candle heights are [3,2,1,3]. The tallest candles are 3 units, and there are 2 of them.'''
+# Candle heights are [3,2,1,3]. The tallest candles are 3 units, and there are 2 of them.
 
 
 #!/bin/python3
 
-import math
+
 import os
-import random
-import re
 import sys
 
 
