bb7a7dff517b2e5a8847bed23ef3dc28df1985e7
fharook shaik <fharookshaik.5@gmail.com>
2020-10-06 10:00:42 +0530

    Final commit of Designer Doot mat problem.

--- Python/DesignerDoorMat.py
+++ Python/DesignerDoorMat.py
@@ -1,3 +1,60 @@
+'''
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be X. ( is an odd natural number, and  is  times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+    
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+Input Format
+
+A single line containing the space separated values of  and .
+
+Constraints
+
+Output Format
+
+Output the design pattern.
+
+Sample Input
+
+9 27
+Sample Output
+
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+
+
+'''
+
 n,m = map(int,input().split(" "))
 str = "WELCOME"
 symbol = ".|."
--- Python/StringSplitAndJoin.py
+++
@@ -1,9 +0,0 @@
-def split_and_join(line):
-    line = line.split(" ")
-    line = "-".join(line)
-    return line
-
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
