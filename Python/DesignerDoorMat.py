87236433e6c74e0dd1851e12b8532986362f20e5
fharook shaik <fharookshaik.5@gmail.com>
2020-10-06 09:36:14 +0530

    Modified the two programs

--- Python/DesignerDoorMat.py
+++ Python/DesignerDoorMat.py
@@ -1,10 +1,13 @@
 n,m = map(int,input().split(" "))
 str = "WELCOME"
 symbol = ".|."
+
 for i in range(0,n):
     if i % 2 != 0:
         print((symbol*i).center(m,"-"))
+
 print(str.center(m,"-"))
+
 i = n-1
 while i>=0:
     if i % 2 != 0:
---
+++ Python/StringSplitAndJoin.py
@@ -0,0 +1,9 @@
+def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
