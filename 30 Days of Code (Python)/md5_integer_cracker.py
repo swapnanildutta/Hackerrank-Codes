c8d80a37ef8529037b39db75678e0a5bd5386dbb
unknown <hosein.fereydonizad@gmail.com>
2020-10-06 17:31:22 +0330

    md5 cracker for numbers

---
+++ 30 Days of Code (Python)/md5_integer_cracker.py
@@ -0,0 +1,25 @@
+import hashlib
+import time
+import random
+import numpy as np
+
+counter = 1
+
+md5_pass = input("Enter md5 password: ")
+
+for i in np.arange(0.0, 11.0, 0.0000001):
+    hash_obj = hashlib.md5(i)
+    start = time.time()
+    print("trying paasword %d -----> %s " % (counter, i))
+    counter += 1
+    end = time.time()
+    t_time = end - start
+
+    if hash_obj == md5_pass:
+        print("Password Found !!! Password is : %s " % i)
+        print("Total Running Time: ", t_time, "Seconds")
+        time.sleep(10)
+        break
+
+else: 
+    print("Password Not Found")
