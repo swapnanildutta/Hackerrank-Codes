6bdae33097451f9c9d636b0876e51adfdb26e944
Hritobroto Sanyal <60534062+hritobroto@users.noreply.github.com>
2020-10-06 22:03:39 +0530

    Added import statement and working main

--- Python/Capitalize!.py
+++ Python/Capitalize!.py
@@ -1,20 +1,36 @@
 '''
 You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
+
 Given a full name, your task is to capitalize the name appropriately.
+
 Input Format
+
 A single line of input containing the full name,
+
 .
+
 Constraints
+
     The string consists of alphanumeric characters and spaces.
+
 Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+
 Output Format
+
 Print the capitalized string,
+
 .
+
 Sample Input
+
 chris alan
+
 Sample Output
+
 Chris Alan
+
 '''
+
 import sys
 
 def solve(s):
