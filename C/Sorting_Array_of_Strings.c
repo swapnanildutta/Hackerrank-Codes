f432f7af7bd231172f83e58380e69e6236209705
abhishekkagautam <abhishekkagautam@gmail.com>
2020-10-06 14:06:11 +0530

    Sorting Array of Strings (Difficulty level hard)

--- C/Sorting_Array_of_Strings.c
+++ C/Sorting_Array_of_Strings.c
@@ -65,6 +65,9 @@ sbv
 wkue
 fekls */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int lexicographic_sort(const char* a, const char* b) {
     return strcmp(a, b);
@@ -111,4 +114,39 @@ void string_sort(char** arr, const int len,int (*cmp_func)(const char* a, const 
             }
         }
     }
+}
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+  
+    char** arr;
+	arr = (char**)malloc(n * sizeof(char*));
+  
+    for(int i = 0; i < n; i++){
+        *(arr + i) = malloc(1024 * sizeof(char));
+        scanf("%s", *(arr + i));
+        *(arr + i) = realloc(*(arr + i), strlen(*(arr + i)) + 1);
+    }
+  
+    string_sort(arr, n, lexicographic_sort);
+    for(int i = 0; i < n; i++)
+        printf("%s\n", arr[i]);
+    printf("\n");
+
+    string_sort(arr, n, lexicographic_sort_reverse);
+    for(int i = 0; i < n; i++)
+        printf("%s\n", arr[i]); 
+    printf("\n");
+
+    string_sort(arr, n, sort_by_length);
+    for(int i = 0; i < n; i++)
+        printf("%s\n", arr[i]);    
+    printf("\n");
+
+    string_sort(arr, n, sort_by_number_of_distinct_characters);
+    for(int i = 0; i < n; i++)
+        printf("%s\n", arr[i]); 
+    printf("\n");
 }
