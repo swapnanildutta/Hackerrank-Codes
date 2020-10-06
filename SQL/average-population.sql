751db85c9468d792faf73117eafd3298f9de732b
pankaj892 <31444506+pankaj892@users.noreply.github.com>
2020-10-06 11:25:12 +0530

    Update average-population.sql

--- SQL/average-population.sql
+++ SQL/average-population.sql
@@ -1,7 +1,6 @@
 -- Average Population of Each Continent
 -- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
-
-Note: CITY.CountryCode and COUNTRY.Code are matching key columns
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns
 -- https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
 
 SELECT country.continent, FLOOR(AVG(city.population))
