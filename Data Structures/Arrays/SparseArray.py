9c78ae738ad19a0ef765fa746ce2c1434e67ded8
Aathira Satheesh <aathirasatheeshnair@gmail.com>
2020-10-06 22:00:56 +0530

    Update SparseArray.py

--- Data Structures/Arrays/SparseArray.py
+++ Data Structures/Arrays/SparseArray.py
@@ -1,4 +1,39 @@
+'''There is a collection of input strings and a collection of query strings. 
+For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+strings=['abc','ab','ab']
+queries=['ab','abc','cc']
+The output will be res=[2,1,0]
 
+Complete the function matchingStrings in the editor below. The function must return an array of integers representing the frequency of occurrence of each query string in strings.
+
+matchingStrings has the following parameters:
+
+-string strings[n] - an array of strings to search
+-string queries[q] - an array of query strings
+Returns
+
+int[q]: an array of results for each query
+Sample Input
+4
+aba
+baba
+aba
+xzxb
+3
+aba
+xzxb
+ab
+------------
+Output
+2
+0
+1
+
+
+
+
+
+'''
 
 
 
@@ -42,5 +77,5 @@ if __name__ == '__main__':
         queries.append(queries_item)
 
     res = matchingStrings(strings, queries)
-
-    print(res)
+    for re in res:
+        print(re)
