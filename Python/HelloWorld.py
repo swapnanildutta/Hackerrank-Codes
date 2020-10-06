282bb17190eb4b369bf643584d530e806c55d37c
DuncanScu <66550169+DuncanScu@users.noreply.github.com>
2020-10-06 16:32:46 +0100

    Adding Hello World Challenge

---
+++ Python/HelloWorld.py
@@ -0,0 +1,16 @@
+'''
+Here is a sample line of code that can be executed in Python:
+print("Hello, World!")
+You can just as easily store a string as a variable and then print it to stdout:
+my_string = "Hello, World!"
+print(my_string)
+
+Now try it youreself to print the following in the console.
+Hello, World!
+'''
+def hello_world():      #Function to print the text
+    print('Hello, World!')
+
+if __name__=="__main__":
+    hello_world()       #using the function in the code
+
