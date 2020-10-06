477099310600d9c8d18ecd4d0477b981d555abe0
Johannes Leppäkorpi <user3610@protonmail.com>
2020-10-06 18:45:01 +0300

    Better formatting

--- CPP/Attribute_Parser.cpp
+++ CPP/Attribute_Parser.cpp
@@ -1,73 +1,73 @@
 /*
-	 This challenge works with a custom-designed markup language HRML. In HRML, each element consists of a starting and ending tag, and there are attributes associated with each tag. Only starting tags can have attributes. We can call an attribute by referencing the tag, followed by a tilde, '~' and the name of the attribute. The tags may also be nested.
+This challenge works with a custom-designed markup language HRML. In HRML, each element consists of a starting and ending tag, and there are attributes associated with each tag. Only starting tags can have attributes. We can call an attribute by referencing the tag, followed by a tilde, '~' and the name of the attribute. The tags may also be nested.
 
-	 The opening tags follow the format:
+The opening tags follow the format:
 
-	 <tag-name attribute1-name = "value1" attribute2-name = "value2" ...>
+<tag-name attribute1-name = "value1" attribute2-name = "value2" ...>
 
-	 The closing tags follow the format:
+The closing tags follow the format:
 
-	 </tag-name>
+</tag-name>
 
-	 The attributes are referenced as:
+The attributes are referenced as:
 
-	 tag1~value
-	 tag1.tag2~name
+tag1~value
+tag1.tag2~name
 
-	 Given the source code in HRML format consisting of
-	 lines, answer
+Given the source code in HRML format consisting of
+lines, answer
 
-	 queries. For each query, print the value of the attribute specified. Print "Not Found!" if the attribute does not exist.
+queries. For each query, print the value of the attribute specified. Print "Not Found!" if the attribute does not exist.
 
-	 Example
+Example
 
-	 HRML listing
-	 <tag1 value = "value">
-	 <tag2 name = "name">
-	 <tag3 another="another" final="final">
-	 </tag3>
-	 </tag2>
-	 </tag1>
+HRML listing
+<tag1 value = "value">
+<tag2 name = "name">
+<tag3 another="another" final="final">
+</tag3>
+</tag2>
+</tag1>
 
-	 Queries
-	 tag1~value
-	 tag1.tag2.tag3~name
-	 tag1.tag2~value
+Queries
+tag1~value
+tag1.tag2.tag3~name
+tag1.tag2~value
 
-	 Here, tag2 is nested within tag1, so attributes of tag2 are accessed as tag1.tag2~<attribute>. Results of the queries are:
+Here, tag2 is nested within tag1, so attributes of tag2 are accessed as tag1.tag2~<attribute>. Results of the queries are:
 
-	 Query                 Value
-	 tag1~value            "value"
-	 tag1.tag2.tag3~name   "Not Found!"
-	 tag1.tag2.tag3~final  "final"
+Query                 Value
+tag1~value            "value"
+tag1.tag2.tag3~name   "Not Found!"
+tag1.tag2.tag3~final  "final"
 
-	 Input Format
+Input Format
 
-	 The first line consists of two space separated integers,
-	 and . specifies the number of lines in the HRML source program.
+The first line consists of two space separated integers,
+and . specifies the number of lines in the HRML source program.
 
-	 specifies the number of queries.
+specifies the number of queries.
 
-	 The following
+The following
 
-	 lines consist of either an opening tag with zero or more attributes or a closing tag. There is a space after the tag-name, attribute-name, '=' and value.There is no space after the last value. If there are no attributes there is no space after tag name.
+lines consist of either an opening tag with zero or more attributes or a closing tag. There is a space after the tag-name, attribute-name, '=' and value.There is no space after the last value. If there are no attributes there is no space after tag name.
 
-	 queries follow. Each query consists of string that references an attribute in the source program.More formally, each query is of the form ~ where and
+queries follow. Each query consists of string that references an attribute in the source program.More formally, each query is of the form ~ where and
 
-	 are valid tags in the input.
+are valid tags in the input.
 
-	 Constraints
+Constraints
 
-	 Each line in the source program contains, at most,
-	 characters.
-	 Every reference to the attributes in the
-	 queries contains at most
+Each line in the source program contains, at most,
+characters.
+Every reference to the attributes in the
+queries contains at most
 
-	 characters.
-	 All tag names are unique and the HRML source program is logically correct, i.e. valid nesting.
-	 A tag can may have no attributes.
+characters.
+All tag names are unique and the HRML source program is logically correct, i.e. valid nesting.
+A tag can may have no attributes.
 
-	 Output Format
+Output Format
 
 Print the value of the attribute for each query. Print "Not Found!" without quotes if the attribute does not exist.
 
