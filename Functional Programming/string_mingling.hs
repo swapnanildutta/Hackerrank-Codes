126b7991c8c822090ed5192d8fd78032268c19bb
Danilo Silva <danilo_silva@ethercity.tech>
2020-10-06 23:57:23 -0300

    Add Haskell String Mingling Problem Description

--- Functional Programming/string_mingling.hs
+++ Functional Programming/string_mingling.hs
@@ -1,3 +1,40 @@
+-- Pawel and Shaka recently became friends. They believe their friendship will last forever if they merge their favorite strings.
+
+-- The lengths of their favorite strings are the same, . Mingling two strings,  and , both of length , will result in the creation of a new string  of length . It will have the following structure:
+
+-- You are given two strings  (Pawel's favorite) and  (Shaka's favorite), determine the mingled string .
+
+-- Input Format
+
+-- The first line of input contains the string .
+-- The second line contains .
+
+-- Output Format
+
+-- Print the mingled string, .
+
+-- Constraints
+
+
+-- The string only consists of lowercase English characters ().
+-- length(P) = length(Q) 
+
+-- Sample Input #00
+
+-- abcde
+-- pqrst
+-- Sample Output #00
+
+-- apbqcrdset
+-- Sample Input #01
+
+-- hacker
+-- ranker
+-- Sample Output #01
+
+-- hraacnkkeerr
+
+
 ming :: String -> String -> String
 ming [] [] = []
 ming (x:xs) (y:ys) = x : y : ming xs ys
