207700168172411989d19a15b58ac03516e5a4a7
Aathira Satheesh <aathirasatheeshnair@gmail.com>
2020-10-07 13:29:27 +0530

    Update Java_Substring.java

--- Java/Java_Substring.java
+++ Java/Java_Substring.java
@@ -1,3 +1,25 @@
+/*
+
+Given a string S, and two indices,start and end, print a substring consisting of all characters in the inclusive range from start to end-1
+
+Input Format
+
+The first line contains a single string denoting s.
+The second line contains two space-separated integers denoting the respective values of start and end
+
+Output Format
+Print the substring in the inclusive range from start to end-1.
+
+Sample Input
+Helloworld
+3 7
+-----------------
+Sample Output
+
+lowo
+
+
+*/
 import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
