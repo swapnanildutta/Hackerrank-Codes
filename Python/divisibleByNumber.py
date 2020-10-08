e46fa3a9c53a942ab2ac7ef32b83fdcb7211730c
LAPTOP-PIGVD7CM\Komal <99kkaur@gmail.com>
2020-10-08 12:36:38 +0530

    divisibe by number

--- Python/divisibleByNumber.py
+++ Python/divisibleByNumber.py
@@ -3,15 +3,16 @@ Task
 Create a list of numbers from 1 to 100 named list_1. The numbers should be present in the increasing order: Ex list_1 = [1,2,3,4,5,....,100]
 Given an input let's say a, you have to print the number of elements of list_1 which are divisible by a,  excluding the element which is equal to a.
 
-Input:
+Input
 Number a
-Output:
+
+Output
 In a single line, the number of elements (i.e. the count and not the elements) which are divisible by a.
 
-Example:
-Input:
+Sample Input
 50
-Output:
+
+Sample Output
 1
 Explanation
 Since there is only one number, i.e. 100 which is divisible by 50 and is in the list_1. We have to exclude the element 50 of list_1 because it is equal to the input.
