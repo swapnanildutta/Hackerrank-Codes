2efd7b3d6d976fded549dd3ea9f123cf18091725
Johannes Leppäkorpi <user3610@protonmail.com>
2020-10-08 08:43:07 +0300

    Improved the standards of the code

--- CPP/Inherited_Code.cpp
+++ CPP/Inherited_Code.cpp
@@ -50,47 +50,41 @@ using namespace std;
 
 /* Define the exception here */
 
-class BadLengthException : public exception{
-    int n;
-
-    public:
-        int what() throw(){
-            return n;
-        }
-        BadLengthException(int number){
-            n = number;
-        }
+struct BadLengthException : public exception {
+    int N;
+    BadLengthException(int n) {N=n;} 
+    int what() {return N;}
 };
 
 bool checkUsername(string username) {
-	bool isValid = true;
-	int n = username.length();
-	if(n < 5) {
-		throw BadLengthException(n);
-	}
-	for(int i = 0; i < n-1; i++) {
-		if(username[i] == 'w' && username[i+1] == 'w') {
-			isValid = false;
-		}
-	}
-	return isValid;
+    bool isValid = true;
+    int n = username.length();
+    if(n < 5) {
+        throw BadLengthException(n);
+    }
+    for(int i = 0; i < n-1; i++) {
+        if(username[i] == 'w' && username[i+1] == 'w') {
+            isValid = false;
+        }
+    }
+    return isValid;
 }
 
 int main() {
-	int T; cin >> T;
-	while(T--) {
-		string username;
-		cin >> username;
-		try {
-			bool isValid = checkUsername(username);
-			if(isValid) {
-				cout << "Valid" << '\n';
-			} else {
-				cout << "Invalid" << '\n';
-			}
-		} catch (BadLengthException e) {
-			cout << "Too short: " << e.what() << '\n';
-		}
-	}
-	return 0;
+    int T; cin >> T;
+    while(T--) {
+        string username;
+        cin >> username;
+        try {
+            bool isValid = checkUsername(username);
+            if(isValid) {
+                cout << "Valid" << '\n';
+            } else {
+                cout << "Invalid" << '\n';
+            }
+        } catch (BadLengthException e) {
+            cout << "Too short: " << e.what() << '\n';
+        }
+    }
+    return 0;
 }
