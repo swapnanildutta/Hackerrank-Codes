280ebbe07461aeee2c5e511a1966b0fc17931cf2
prackode <72190853+prackode@users.noreply.github.com>
2020-10-09 12:20:56 +0530

    Update Climbing_the_Leaderboard.py

--- Python/Climbing_the_Leaderboard.py
+++ Python/Climbing_the_Leaderboard.py
@@ -1,4 +1,6 @@
+
 '''
+
 An arcade game player wants to climb to the top of the leaderboard and track their ranking. The game uses Dense Ranking, so its leaderboard works like this:
 The player with the highest score is ranked number 1 on the leaderboard.
 Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
@@ -28,6 +30,7 @@ The player's scores, , are in ascending order.
 Subtask
 
 For  of the maximum score:
+
 '''
 
 def climbingLeaderboard(scores, alice):
