39d24f032b602877448f7d1dfc14014abd02cdcd
prackode <72190853+prackode@users.noreply.github.com>
2020-10-09 12:16:49 +0530

    Update Climbing_the_Leaderboard.py

--- Python/Climbing_the_Leaderboard.py
+++ Python/Climbing_the_Leaderboard.py
@@ -1,40 +1,33 @@
 '''
-
 An arcade game player wants to climb to the top of the leaderboard and track their ranking. The game uses Dense Ranking, so its leaderboard works like this:
-
 The player with the highest score is ranked number 1 on the leaderboard.
 Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
 Example
 * ranked=[100,90,90,80]
 * player=[70,80,105]
 The ranked players will have ranks 1, 2, 2, and 3, respectively. If the player's scores are 70, 80 and 105, their rankings after each game are 4th, 3rd and 1st. Return [4,3,1].
-
+.
 Function Description
-
 Complete the climbingLeaderboard function in the editor below.
-
 climbingLeaderboard has the following parameter(s):
-
 int ranked[n]: the leaderboard scores
 int player[m]: the player's scores
 Returns
-
 int[m]: the player's rank after each new score
+.
 Input Format
 
 The first line contains an integer , the number of players on the leaderboard.
 The next line contains  space-separated integers , the leaderboard scores in decreasing order.
 The next line contains an integer, , the number games the player plays.
 The last line contains  space-separated integers , the game scores.
-
+.
 Constraints
-
 The existing leaderboard, , is in descending order.
 The player's scores, , are in ascending order.
 Subtask
 
 For  of the maximum score:
-
 '''
 
 def climbingLeaderboard(scores, alice):
