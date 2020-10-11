cf95636ad00a7b218d89661bf53a3425403b6080
Varshini V <57315334+okvarsh@users.noreply.github.com>
2020-10-11 20:25:01 +0530

    Update BeautifulBinaryString.py

--- Problem Solving/BeautifulBinaryString.py
+++ Problem Solving/BeautifulBinaryString.py
@@ -1,5 +1,6 @@
 """
-Alice has a binary string. She thinks a binary string is beautiful if and only if it doesn't contain the substring . In one step, Alice can change a  to a  or vice versa. Count and print the minimum number of steps needed to make Alice see the string as beautiful. For example, if Alice's string is  she can change any one element and have a beautiful string.
+Alice has a binary string. She thinks a binary string is beautiful if and only if it doesnt contain the substring . 
+In one step, Alice can change a  to a  or vice versa. Count and print the minimum number of steps needed to make Alice see the string as beautiful. For example, if Alices string is  she can change any one element and have a beautiful string.
 
 Function Description: Complete the beautifulBinaryString function in the editor below. It should return an integer representing the minimum moves required.
 
