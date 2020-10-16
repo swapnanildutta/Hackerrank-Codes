6996a71a5dbd33e8d347d381f9c0c458717abc96
SudhirPawar <pawarsudhir8498@gmail.com>
2020-10-16 19:13:14 +0530

    Update and Rename PickingNumbers.java

--- Problem Solving/Picking-Numbers.java
+++ Problem Solving/Picking-Numbers.java
@@ -53,15 +53,8 @@ We choose the following multiset of integers from the array: {1,2,2,1,2}. Each p
 
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
@@ -121,7 +114,7 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+//         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
             .map(Integer::parseInt)
