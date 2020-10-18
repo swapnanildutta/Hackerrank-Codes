702dea7b55484d8d23bbb7b3c1c3d115a09f54be
saranshkotnala <66181092+saranshkotnala@users.noreply.github.com>
2020-10-18 23:05:50 +0530

    Update Piling_up.py

--- Python/Piling_up.py
+++ Python/Piling_up.py
@@ -1,71 +1,52 @@
-'''There is a horizontal row of  cubes. The length of each cube is given. You need to create a new vertical pile of cubes. The new pile should follow these directions: if  is on top of  then .
+#There is a horizontal row of  cubes. The length of each cube is given. You need to create a new vertical pile of cubes. The new pile should follow these directions: if  is on top of  then .
 
-When stacking the cubes, you can only pick up either the leftmost or the rightmost cube each time. Print "Yes" if it is possible to stack the cubes. Otherwise, print "No". Do not print the quotation marks.
+#When stacking the cubes, you can only pick up either the leftmost or the rightmost cube each time. Print "Yes" if it is possible to stack the cubes. Otherwise, print "No". Do not print the quotation marks.
 
-Input Format
+#Input Format
 
-The first line contains a single integer , the number of test cases.
-For each test case, there are  lines.
-The first line of each test case contains , the number of cubes.
-The second line contains  space separated integers, denoting the sideLengths of each cube in that order.
+#The first line contains a single integer , the number of test cases.
+#For each test case, there are  lines.
+#The first line of each test case contains , the number of cubes.
+#The second line contains  space separated integers, denoting the sideLengths of each cube in that order.
 
-Constraints
+#Constraints
 
 
 
-Output Format
+#Output Format
 
-For each test case, output a single line containing either "Yes" or "No" without the quotes.
+#For each test case, output a single line containing either "Yes" or "No" without the quotes.
 
-Sample Input
+#Sample Input
 
-2
-6
-4 3 2 1 3 4
-3
-1 3 2
-Sample Output
+#2
+#6
+#4 3 2 1 3 4
+#3
+#1 3 2
+#Sample Output
 
-Yes
-No
-Explanation
+#Yes
+#No
+#Explanation
 
-In the first test case, pick in this order: left -4 , right -4 , left -3 , right - 3, left -2 , right -1 .
-In the second test case, no order gives an appropriate arrangement of vertical cubes.  will always come after either  or . '''
+#In the first test case, pick in this order: left -4 , right -4 , left -3 , right - 3, left -2 , right -1 .
+#In the second test case, no order gives an appropriate arrangement of vertical cubes.  will always come after either  or . '''
 
-from collections import deque
+def Solution(lst):
+  pile = float('inf')
+  while lst:
+    num = lst.pop(0) if lst[0] > lst[-1] else lst.pop(-1)
+    if num > pile: return "No"
+    pile = num
+  return "Yes"
 
-N = int(input())
+def main():
+  t = int(input())
+  for _ in range(t):
+    n = int(input())
+    lst = list(map(int, input().strip().split()))
+    print(Solution(lst))
 
-for _ in range(N):
-    flag = True
-    input()
-    d = deque(map(int, input().strip().split()))
-    if(d[0] >= d[-1]):
-        max = d.popleft()
-    else:
-        max = d.pop()
-    while d:
-        if(len(d)==1):
-            if(d[0] <= max):
-                break
-            else:
-                flag = False
-                break
-        else:
-            if(d[0]<=max and d[-1]<=max):
-                if(d[0]>=d[-1]):
-                    max = d.popleft()
-                else:
-                    max = d.pop()
-            elif(d[0]<=max):
-                max = d.popleft()
-            elif(d[-1]<=max):
-                max = d.pop()
-            else:
-                flag = False
-                break
-    if flag:
-        print("Yes")
-    else:
-        print("No")
+if __name__ == "__main__":
+  main()
