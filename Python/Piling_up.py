dd871513458dd88759fec16028875d5765b9dd20
saranshkotnala <66181092+saranshkotnala@users.noreply.github.com>
2020-10-18 23:33:31 +0530

    Update Piling_up.py

--- Python/Piling_up.py
+++ Python/Piling_up.py
@@ -46,7 +46,8 @@ def Solution(lst):
   pile = float('inf')
   while lst:
     num = lst.pop(0) if lst[0] > lst[-1] else lst.pop(-1)
-    if num > pile: return "No"
+    if num > pile:
+      return "No"
     pile = num
   return "Yes"
 def main():
