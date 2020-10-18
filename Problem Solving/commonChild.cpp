5b96a7d29719a19f2cb9c3cbcf5d0f7681ef0813
avnoor-488 <avnoorsingh488@gmail.com>
2020-10-18 14:41:23 +0530

    update Files with ques

--- Problem Solving/commonChild.cpp
+++ Problem Solving/commonChild.cpp
@@ -1,3 +1,9 @@
+// A string is said to be a child of a another string if it can be formed by deleting 0 or more characters from the other string. Given two strings of equal length, what's the longest string that can be constructed such that it is a child of both?
+
+// For example, ABCD and ABDC have two children with maximum length 3, ABC and ABD. They can be formed by eliminating either the D or C from both strings. Note that we will not consider ABCD as a common child because we can't rearrange characters and ABCD  ABDC.
+
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
