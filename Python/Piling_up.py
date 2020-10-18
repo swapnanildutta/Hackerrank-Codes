78cc4e12444b004fdd12dc33597cf9a1be3de37e
saranshkotnala <66181092+saranshkotnala@users.noreply.github.com>
2020-10-18 23:11:39 +0530

    Update Piling_up.py

--- Python/Piling_up.py
+++ Python/Piling_up.py
@@ -1,13 +1,19 @@
-#There is a horizontal row of  cubes. The length of each cube is given. You need to create a new vertical pile of cubes. The new pile should follow these directions: if  is on top of  then .
+#There is a horizontal row of  cubes. The length of each cube is given. 
+#You need to create a new vertical pile of cubes. 
+#The new pile should follow these directions: if  is on top of  then .
 
-#When stacking the cubes, you can only pick up either the leftmost or the rightmost cube each time. Print "Yes" if it is possible to stack the cubes. Otherwise, print "No". Do not print the quotation marks.
+#When stacking the cubes, 
+#you can only pick up either the leftmost or the rightmost cube each time. 
+#Print "Yes" if it is possible to stack the cubes.Otherwise, print "No". 
+#Do not print the quotation marks.
 
 #Input Format
 
 #The first line contains a single integer , the number of test cases.
 #For each test case, there are  lines.
 #The first line of each test case contains , the number of cubes.
-#The second line contains  space separated integers, denoting the sideLengths of each cube in that order.
+#The second line contains  space separated integers,
+#denoting the sideLengths of each cube in that order.
 
 #Constraints
 
@@ -15,7 +21,8 @@
 
 #Output Format
 
-#For each test case, output a single line containing either "Yes" or "No" without the quotes.
+#For each test case, output a single line containing either
+#"Yes" or "No" without the quotes.
 
 #Sample Input
 
@@ -30,8 +37,10 @@
 #No
 #Explanation
 
-#In the first test case, pick in this order: left -4 , right -4 , left -3 , right - 3, left -2 , right -1 .
-#In the second test case, no order gives an appropriate arrangement of vertical cubes.  will always come after either  or . '''
+#In the first test case, pick in this order:
+#left -4 , right -4 , left -3 , right - 3, left -2 , right -1 .
+#In the second test case, no order gives an appropriate
+#arrangement of vertical cubes.  will always come after either  or . 
 
 def Solution(lst):
   pile = float('inf')
@@ -40,7 +49,6 @@ def Solution(lst):
     if num > pile: return "No"
     pile = num
   return "Yes"
-
 def main():
   t = int(input())
   for _ in range(t):
