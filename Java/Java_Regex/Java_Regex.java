69b4b7729271c444891745da6439ff7509de1617
VishnuRameshbabu <vishnuramesh200@gmail.com>
2020-10-21 16:12:33 +0530

    Fixed the format

--- Java/Java_Regex/Java_Regex.java
+++ Java/Java_Regex/Java_Regex.java
@@ -42,7 +42,20 @@ false
 false
 false
 */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String sc=in.next();
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
 
+    }
+}
 class MyRegex{
   String check = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
 public String pattern = check + "\\." + check + "\\." + check + "\\." + check;
