b93312de949ac80ab9cc3479e9f2aeca1f93e093
Aby-Sebastian <abysebastian98@gmail.com>
2020-10-21 22:10:02 +0530

    add A Personalized Echo

---
+++ Linux Shell/A Personalized Echo.sh
@@ -0,0 +1,14 @@
+#Write a Bash script which accepts name as input and displays a greeting: "Welcome (name)"
+
+#Input Format
+
+#One line, containing a name.
+
+#Output Format
+
+#One line: "Welcome (name)" (quotation marks excluded).
+#The evaluation will be case-sensitive.
+
+read name
+echo "Welcome $name"
+
