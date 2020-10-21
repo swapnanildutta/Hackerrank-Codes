9017c055f4d8c9e6b5fd462488279002e3d24cf1
Aby-Sebastian <abysebastian98@gmail.com>
2020-10-21 22:18:08 +0530

    add Comparing Numbers and Looping with Numbers

---
+++ Linux Shell/Comparing Numbers.sh
@@ -0,0 +1,28 @@
+#Given two integers,  and , identify whether  or  or .
+
+#Exactly one of the following lines:
+#- X is less than Y
+#- X is greater than Y
+#- X is equal to Y
+
+#Input Format
+
+#Two lines containing one integer each (X and Y, respectively).
+
+#Output Format
+
+#Exactly one of the following lines:
+#- X is less than Y
+#- X is greater than Y
+#- X is equal to Y
+
+read firstNumber
+read secondNumber
+if (($firstNumber > $secondNumber)); then
+    echo X is greater than Y;
+elif (($firstNumber < $secondNumber)); then
+    echo X is less than Y;
+else
+    echo X is equal to Y;
+fi
+
---
+++ Linux Shell/Looping with Numbers.sh
@@ -0,0 +1,24 @@
+#Use a for loop to display the natural numbers from 1 to 50.
+
+#Input Format
+
+#There is no input
+
+#Output Format
+
+#1
+#2
+#3
+#4
+#5
+#.
+#.
+#50
+
+X=1
+while [ $X -le 50 ]
+do
+        echo $X
+        X=$((X+1))
+done
+
