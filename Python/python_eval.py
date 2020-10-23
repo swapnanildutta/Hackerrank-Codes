a122308b837ca84e523ad8f26c27eb7bee2dd52c
Aashish Pawar ⚡ <pawarashish564@gmail.com>
2020-10-23 19:43:49 +0530

    Update python_eval.py

--- Python/python_eval.py
+++ Python/python_eval.py
@@ -1,2 +1,3 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+# You are given an expression in a line. Read that line as a string variable, such as var, and print the result using eval(var).
+
 eval(input()) 
