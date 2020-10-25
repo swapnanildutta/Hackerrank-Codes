9fc7b530907ef29cad87d7a65de0b9b148f374d1
Aashish Pawar ⚡ <pawarashish564@gmail.com>
2020-10-25 10:10:19 +0530

    Update python_eval.py

--- Python/python_eval.py
+++ Python/python_eval.py
@@ -1,3 +1,20 @@
-# You are given an expression in a line. Read that line as a string variable, such as var, and print the result using eval(var).
+'''
+Objective 
+The eval() expression is a very powerful built-in function of Python. It helps in evaluating an expression. The expression can be a Python statement, or a code object.
 
+Task
+You are given an expression in a line. Read that line as a string variable, such as var, and print the result using eval(var).
+
+NOTE: Python2 users, please import from __future__ import print_function.
+
+Constraint
+Input string is less than 100 characters.
+
+Sample Input
+print(2 + 3)
+
+Sample Output
+5
+
+'''
 eval(input()) 
