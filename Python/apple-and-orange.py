4d4eff68550a5821217169a6411a91fa669e1f7e
Brent Schuetze <u5905548@anu.edu.au>
2020-10-27 11:47:39 +1030

    Remove unused imports and tidy whitespace

--- Python/apple-and-orange.py
+++ Python/apple-and-orange.py
@@ -1,13 +1,9 @@
 '''
+
 Apples and Oranges
 https://www.hackerrank.com/challenges/apple-and-orange/problem
-'''
 
-import math
-import os
-import random
-import re
-import sys
+'''
 
 # Complete the countApplesAndOranges function below.
 def countApplesAndOranges(s, t, a, b, apples, oranges):
