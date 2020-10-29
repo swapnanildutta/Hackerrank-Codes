07db6f1cdc7e75c12e0056432db2b8b221beb213
SudhirPawar <pawarsudhir8498@gmail.com>
2020-10-29 11:06:33 +0530

    Update FindDigits.py

--- Problem Solving/Python/FindDigits.py
+++ Problem Solving/Python/FindDigits.py
@@ -40,17 +40,11 @@ Explanation
 The number 12 is broken into two digits, 1 and 2. When 12 is divided by either of those two digits, the remainder is 0 so they are both divisors.
 
 The number 1012 is broken into four digits, 1, 0, 1, and 2. 1012 is evenly divisible by its digits 1, 1, and 2, but it is not divisible by 0 as division by zero is undefined.
-
-
 '''
 
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
 
 # Complete the findDigits function below.
 
