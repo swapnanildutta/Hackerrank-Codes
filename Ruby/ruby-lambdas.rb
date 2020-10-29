a1c10b6ff3f8065d4917c8528c3332b37b8580a5
I-Ju Lin <9304486+lin826@users.noreply.github.com>
2020-10-29 19:28:44 +0800

    Remove existing code from the problem
    Fix and avoid error from Codacy/PR Quality Review

--- Ruby/ruby-lambdas.rb
+++ Ruby/ruby-lambdas.rb
@@ -86,13 +86,3 @@ adder       = ->(a, b) { a + b }
 
 # Write a lambda which takes a hash and returns an array of hash values
 values_only = ->(a) { a.values }
-
-
-input_number_1 = gets.to_i
-input_number_2 = gets.to_i
-input_hash = eval(gets)
-
-a = square.(input_number_1); b = plus_one.(input_number_2);c = into_2.(input_number_1); 
-d = adder.(input_number_1, input_number_2);e = values_only.(input_hash)
-
-p a; p b; p c; p d; p e
